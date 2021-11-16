a43e38aa1248f56d102869fcc5836fdfe1c2c3ff
Rangel Ivanov <rangel.ivanov@sap.com>
2021-11-16 11:58:26 +0200

    Add sticky connections test

---
+++ concourse/tests/sticky-connections.sh
@@ -0,0 +1,8 @@
+for ((i=0; i<3; i++)); do
+    echo "ping 1" | nc -q 0 127.0.0.1 80
+    sleep 2
+    echo "ping 2" | nc -q 0 127.0.0.1 80
+    sleep 2
+    echo "ping 3" | nc -q 0 127.0.0.1 80
+    sleep 2
+done
